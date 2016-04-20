6d35b1befdb7c85f32c080092670c93a63f68c6e
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-04-19 22:51:33 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="iwx",
-    version='0.1.0-dev',
+    version='0.1.0',
     description='wxPython custom controls',
     author='Pedro Jorge De Los Santos',
     author_email='delossantosmfq@gmail.com',
