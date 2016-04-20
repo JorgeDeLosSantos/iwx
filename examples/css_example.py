dcfec92db98a27d15177fb38d3744556b97c785d
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-04-20 17:41:30 -0500

    CSS Example

---
+++ examples/css_example.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+CSS Styles for wxPython controls
+"""
+import wx
+import iwx.css as css
+
+def test():
+    fr = wx.Frame(None, -1, "Test Frame", size=(300,200))
+    bt = wx.Button(fr, -1, "Button")
+    sz = wx.BoxSizer(wx.VERTICAL)
+    sz.Add(bt, 1, wx.EXPAND|wx.ALL, 10)
+    fr.SetSizer(sz)
+    css.SetControlStyleSheet(bt, "#self{font-size: 16px; background-color: #ff00ff;}")
+    css.SetControlStyleSheet(fr, "#self{background-color: #fafafa;}")
+    fr.Show()
+
+if __name__ == '__main__':
+    app = wx.App()
+    test()
+    app.MainLoop()
