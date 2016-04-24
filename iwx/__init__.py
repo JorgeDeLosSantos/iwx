5f23ccb6e8dbf233b182cac90cf867bb758d3751
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-04-23 23:42:14 -0500

    Added some info pack

--- iwx/__init__.py
+++ iwx/__init__.py
@@ -2,3 +2,14 @@
 import warnings
 
 warnings.warn("Unstable version. Currently in development.")
+
+__version__ = '0.1.0'
+__author__ = 'Pedro Jorge De Los Santos'
+__email__ = 'delossantosmfq@gmail.com'
+
+__info__ = """
+For more information, please visit the iwx GitHub repository
+
+URL: http://github.com/JorgeDeLosSantos/iwx
+"""
+
